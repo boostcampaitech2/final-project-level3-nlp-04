07f9bdd07889385c572d173a67aea5d9e2f649cf
raki-1203 <jfhdzzang@gmail.com>
2021-12-06 02:35:30 +0900

    error message 안나오게 수정

--- new_review_crawling.py
+++ new_review_crawling.py
@@ -13,12 +13,8 @@ from selenium.common.exceptions import TimeoutException
 import pandas as pd
 import time
 from bs4 import BeautifulSoup
-import chromedriver_autoinstaller
-
-
-# 드라이버 프로그램이 설치되어있는 경로
-chromedriver = './chromedriver'
-
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 chrome_options = webdriver.ChromeOptions()
 # 서버에서 실행 시 수행
@@ -27,19 +23,17 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument("--single-process")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
-print(chromedriver_autoinstaller.install())
-
-driver = webdriver.Chrome(executable_path=chromedriver_autoinstaller.install(), options=chrome_options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
 # input_target으로 들어온 역의 주소를 찾는 함수
 
 def search_address(target_station, target_input_address, sort_dist_flag):
     # 입력할 search의 form과 button을 찾는다.
-    input_target = target_input_address.strip() #지번
-    input_station = target_station.strip() #역이름+2호선
-    search_text = driver.find_element_by_css_selector('#search > div > form > input')
-    search_button = driver.find_element_by_css_selector('#button_search_address > button.btn.btn-default.ico-pick')
-    cancel_button = driver.find_element_by_css_selector('#button_search_address > button.btn-search-location-cancel.btn-search-location.btn.btn-default > span')
+    input_target = target_input_address.strip()  # 지번
+    input_station = target_station.strip()  # 역이름+2호선
+    search_text = driver.find_element(By.CSS_SELECTOR, '#search > div > form > input')
+    search_button = driver.find_element(By.CSS_SELECTOR, '#button_search_address > button.btn.btn-default.ico-pick')
+    cancel_button = driver.find_element(By.CSS_SELECTOR, '#button_search_address > button.btn-search-location-cancel.btn-search-location.btn.btn-default > span')
     prev_url = driver.current_url
     search_keyword = input_target
 
@@ -48,10 +42,10 @@ def search_address(target_station, target_input_address, sort_dist_flag):
             # driver.execute_script('arguments[0].click();', cancel_button)
             search_text.clear()
             time.sleep(1)
-            # sort_selector = driver.find_element_by_class_name("list-option-inner").find_element_by_class_name("form-control").find_element_by_css_selector('option:nth-child(5)').click()
-            # sort_selector = driver.find_element_by_css_selector('#content > div > div.row.restaurant-list-info > div.list-option > div > select > option:nth-child(5)')
+            # sort_selector = driver.find_element_by_class_name("list-option-inner").find_element_by_class_name("form-control").find_element(By.CSS_SELECTOR, 'option:nth-child(5)').click()
+            # sort_selector = driver.find_element(By.CSS_SELECTOR, '#content > div > div.row.restaurant-list-info > div.list-option > div > select > option:nth-child(5)')
             # 입력 form에 input_target을 넣고 검색을 한다.
-            # driver.find_element_by_css_selector('#search > div > form > input').click()
+            # driver.find_element(By.CSS_SELECTOR, '#search > div > form > input').click()
             search_text.send_keys(search_keyword)
             driver.execute_script("arguments[0].click();", search_button)
             # search_text.send_keys(Keys.ENTER)
@@ -67,22 +61,22 @@ def search_address(target_station, target_input_address, sort_dist_flag):
             time.sleep(1)
             name, num = input_station.split()
 
-            if i ==1:
-                search_keyword = name+num
-                search_text.send_keys(search_keyword) #역이름+호선이름 입력 (붙여쓰기)
-            elif i==2:
-                search_keyword = name+' '+num
-                search_text.send_keys(search_keyword) #역이름+호선이름 입력 (띄어쓰기)
-            elif i==3:
+            if i == 1:
+                search_keyword = name + num
+                search_text.send_keys(search_keyword)  # 역이름+호선이름 입력 (붙여쓰기)
+            elif i == 2:
+                search_keyword = name + ' ' + num
+                search_text.send_keys(search_keyword)  # 역이름+호선이름 입력 (띄어쓰기)
+            elif i == 3:
                 search_keyword = name
-                search_text.send_keys(search_keyword) #역이름
+                search_text.send_keys(search_keyword)  # 역이름
 
             driver.execute_script("arguments[0].click();", search_button)
             # search_text.send_keys(Keys.ENTER)
             time.sleep(2)
 
         try:
-            if driver.find_element_by_css_selector('#search > div > form > ul > li:nth-child(1) > a').text == "검색하신 주소를 찾을 수 없습니다.":
+            if driver.find_element(By.CSS_SELECTOR, '#search > div > form > ul > li:nth-child(1) > a').text == "검색하신 주소를 찾을 수 없습니다.":
                 if i == 3:
                     return sort_dist_flag, True, None
                 continue
@@ -117,7 +111,7 @@ def search_address(target_station, target_input_address, sort_dist_flag):
             break
 
     if not sort_dist_flag:
-        driver.find_element_by_css_selector('#content > div > div.row.restaurant-list-info > div.list-option > div > select > option:nth-child(5)').click()
+        driver.find_element(By.CSS_SELECTOR, '#content > div > div.row.restaurant-list-info > div.list-option > div > select > option:nth-child(5)').click()
         time.sleep(5)
         # driver.get(url)
         # time.sleep(5)
@@ -125,14 +119,15 @@ def search_address(target_station, target_input_address, sort_dist_flag):
 
     return sort_dist_flag, False, search_keyword
 
+
 # 리뷰 크롤링하는 함수, 이 부분의 크롤링 할 대상을 수정해야 한다. <- 현재는 재영님이 사용한 코드를 사용하는 중
 def review_crawling(target_station, target_address, target_category):
     count = 0
     loop = True
-    review_button = driver.find_element_by_css_selector('#content > div.restaurant-detail.row.ng-scope > div.col-sm-8 > ul > li:nth-child(2) > a')
+    review_button = driver.find_element(By.CSS_SELECTOR, '#content > div.restaurant-detail.row.ng-scope > div.col-sm-8 > ul > li:nth-child(2) > a')
     driver.execute_script("arguments[0].click();", review_button)
     time.sleep(5)
-    total_review_num = int(driver.find_element_by_css_selector('#content > div.restaurant-detail.row.ng-scope > div.col-sm-8 > ul > li.active > a > span').text)
+    total_review_num = int(driver.find_element(By.CSS_SELECTOR, '#content > div.restaurant-detail.row.ng-scope > div.col-sm-8 > ul > li.active > a > span').text)
     # 리뷰 '더 보기' 클릭 ('더 보기'가 없거나 20번 누르면 스탑)
 
     while loop and count < 10:
@@ -146,7 +141,7 @@ def review_crawling(target_station, target_address, target_category):
             element = WebDriverWait(driver, 5).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, '#review > li.list-group-item.btn-more > a'))
             )
-            more_button = driver.find_element_by_css_selector('#review > li.list-group-item.btn-more > a')
+            more_button = driver.find_element(By.CSS_SELECTOR, '#review > li.list-group-item.btn-more > a')
             driver.execute_script("arguments[0].click();", more_button)
             count += 1
             time.sleep(1.5)
@@ -162,14 +157,14 @@ def review_crawling(target_station, target_address, target_category):
                                             "ng-bind": "restaurant.name"}).text.strip()
 
     # 최소주문금액
-    min_cost = driver.find_element_by_css_selector("#content > div.restaurant-detail.row.ng-scope > div.col-sm-8 > div.restaurant-info > div.restaurant-content > ul > li:nth-child(3) > span").text
-    
+    min_cost = driver.find_element(By.CSS_SELECTOR, "#content > div.restaurant-detail.row.ng-scope > div.col-sm-8 > div.restaurant-info > div.restaurant-content > ul > li:nth-child(3) > span").text
+
     # 여기서 얻은 html소스로 리뷰, 메뉴, 별점
     restaurant_review = html_source.find_all("li", attrs={"class": "list-group-item star-point ng-scope",
                                                           "ng-repeat": "review in restaurant.reviews"})
 
     for review_index, i in enumerate(restaurant_review):
-        try :
+        try:
             user_id = i.find("span", attrs={"class": "review-id ng-binding",
                                             "ng-show": "review.phone"}).text.strip()
             time_info = i.find("span", attrs={"class": "review-time ng-binding",
@@ -186,9 +181,8 @@ def review_crawling(target_station, target_address, target_category):
             quantity_star = i.find("span", attrs={"class": "points ng-binding",
                                                   "ng-show": "review.rating_quantity > 0"}).text.strip()
 
-            delivery_star = '-1' if i.find("span", attrs={"class": "points ng-binding",
-                                                  "ng-show": "review.rating_delivery > 0"}) == None else i.find("span", attrs={"class": "points ng-binding",
-                                                  "ng-show": "review.rating_delivery > 0"}).text.strip()
+            delivery_star = '-1' if i.find("span", attrs={"class": "points ng-binding", "ng-show": "review.rating_delivery > 0"}) == None else i.find("span", attrs={"class": "points ng-binding",
+                               "ng-show": "review.rating_delivery > 0"}).text.strip()
 
             cur_time = time.time()
             if "시간" in time_info:
@@ -215,9 +209,8 @@ def review_crawling(target_station, target_address, target_category):
 
             written_review = f"{written_review[0]}-{written_review[1]}-{written_review[2]}"
 
-
             try:
-                image= i.select('#review > li:nth-child('+ str(review_index+2) +') > table')
+                image = i.select('#review > li:nth-child(' + str(review_index + 2) + ') > table')
                 image_urls = image[0].find_all("img")
                 image_list = []
                 for image_url in image_urls:
@@ -241,24 +234,26 @@ def review_crawling(target_station, target_address, target_category):
             print(f"image_url: {image_list})")
             print("\n")
 
-
-            main_list.append([brand, target_station.split()[1], target_address, user_id, written_review, review, menu, star, taste_star, \
-                         quantity_star, delivery_star, image_list])
-        except :
+            main_list.append(
+                [brand, target_station.split()[1], target_address, user_id, written_review, review, menu, star,
+                 taste_star, quantity_star, delivery_star, image_list])
+        except:
             continue
 
 
 def click_category(target):
     index = category_name.index(target) + 3
     print(target, index)
-    category_button = driver.find_element_by_css_selector('#category > ul > li:nth-child('+str(index)+')')
+    category_button = driver.find_element(By.CSS_SELECTOR, '#category > ul > li:nth-child(' + str(index) + ')')
     driver.execute_script("arguments[0].click();", category_button)
     time.sleep(5)
+
 
 # 식당을 클릭하고 review_crawling 함수를 통해서 해당 식당을 크롤링한다.
 def click_restaurant(target_station, target_address, target_category):
     # 현재 페이지에서 요기요 등록점 식당에 대해서 정보를 얻어온다. test할때는 한 번에 50개 정도의 식당정보가 나왔습니다.
-    restaurant_list = driver.find_element_by_css_selector('#content > div > div:nth-child(5) > div > div').text.split('\n\n\n\n')
+    restaurant_list = driver.find_element(By.CSS_SELECTOR, '#content > div > div:nth-child(5) > div > div').text.split(
+        '\n\n\n\n')
     number_of_restaurant = len(restaurant_list)
     print(number_of_restaurant)
 
@@ -266,8 +261,9 @@ def click_restaurant(target_station, target_address, target_category):
     prev_url = driver.current_url
 
     # 위에서 얻어온 식당 정보를 바탕으로 첫번째 식당부터 하나씩 클릭해서 페이지에 접근하기 + 접근한 식당 페이지에서 크롤링하기
-    for i in range(1, number_of_restaurant+1):
-        target_restaurant_name = restaurant_list[i-1].split()[0]
+    # for i in range(1, number_of_restaurant+1):
+    for i in range(1, 2):
+        target_restaurant_name = restaurant_list[i - 1].split()[0]
         if category_dict.get(target_restaurant_name) != None:
             category_value = category_dict[target_restaurant_name]
             if target_category not in category_value:
@@ -275,7 +271,7 @@ def click_restaurant(target_station, target_address, target_category):
             continue
 
         category_dict[target_restaurant_name] = [target_category]
-        target_restaurant = driver.find_element_by_css_selector('#content > div > div:nth-child(5) > div > div > div:nth-child('+str(i)+') > div' )
+        target_restaurant = driver.find_element(By.CSS_SELECTOR, '#content > div > div:nth-child(5) > div > div > div:nth-child(' + str(i) + ') > div')
         target_restaurant.click()
         time.sleep(2)
         review_crawling(target_station, target_address, target_category)
@@ -283,8 +279,9 @@ def click_restaurant(target_station, target_address, target_category):
         driver.get(prev_url)
         time.sleep(3)
 
+
 def address_page(target_station, target_address, sort_dist_flag, skip_flag):
-    cancel_button = driver.find_element_by_css_selector('#button_search_address > button.btn-search-location-cancel.btn-search-location.btn.btn-default > span')
+    cancel_button = driver.find_element(By.CSS_SELECTOR, '#button_search_address > button.btn-search-location-cancel.btn-search-location.btn.btn-default > span')
 
     sort_dist_flag, skip_flag, search_address_keyword = search_address(target_station, target_address, sort_dist_flag)
 
@@ -296,12 +293,13 @@ def address_page(target_station, target_address, sort_dist_flag, skip_flag):
 
     return sort_dist_flag, skip_flag
 
+
 # 크롤링을 정보를 담기 위한 main_list
 # main_dict = dict()
 main_list = []
 category_dict = dict()
 url = 'https://www.yogiyo.co.kr/mobile/#/%EC%84%9C%EC%9A%B8%ED%8A%B9%EB%B3%84%EC%8B%9C/135081/'
-category_name = ['1인분 주문']#, '프랜차이즈', '치킨', '피자/양식', '중국집', '한식', '일식/돈까스', '족발/보쌈', '야식', '분식', '카페/디저트']
+category_name = ['1인분 주문']  # , '프랜차이즈', '치킨', '피자/양식', '중국집', '한식', '일식/돈까스', '족발/보쌈', '야식', '분식', '카페/디저트']
 # driver = webdriver.Chrome('./chromedriver')
 driver.get(url)
 response = requests.get(url)
@@ -324,23 +322,24 @@ if response.status_code == 200:
     target_station_address = subway_data['address'].to_list()
     # 지하철 역을 주소로 주면서 search address 반보고하기
     # for station, address in zip(target_statation, target_station_address):
-    for station, address in zip(['신도림','역삼','강남'], ['신도림동 460-26', '역삼동 804','역삼동 858']):
-        sort_dist_flag, skip_flag = address_page(station+'역 '+subway_number2, address, sort_dist_flag, skip_flag)
+    for station, address in zip(['신도림', '역삼', '강남'], ['신도림동 460-26', '역삼동 804', '역삼동 858']):
+        sort_dist_flag, skip_flag = address_page(station + '역 ' + subway_number2, address, sort_dist_flag, skip_flag)
         skip_flag = False
 
     df_main = pd.DataFrame(main_list)
-    df_category = pd.DataFrame.from_dict(category_dict).T.rename(columns={0 :'category_name'})
+    category_dict = {k: ', '.join(v) for k, v in category_dict.items()}
+    df_category = pd.DataFrame.from_dict(category_dict, orient='index').rename(columns={0: 'category_name'})
+    df_category = df_category.reset_index().rename(columns={'index': 'restuarant_name'})
     # df_category['restuarant_name'] = df_category.index
-    df_main.rename(columns={0 : 'restuarant_name', 1 : 'subway', 2 : 'address', 3 : 'user_id', \
-                            4 : 'review_create_time', 5 : 'review_context', 6 : 'menu', \
-                            7 : 'total_star', 8 : 'taste_star', 9 : 'quantity_star', \
-                            10 : 'delivery_star', 11 : 'image_url'}, inplace=True)
-    df_main.set_index('restuarant_name', inplace=True)
+    df_main.rename(columns={0: 'restuarant_name', 1: 'subway', 2: 'address', 3: 'user_id',
+                            4: 'review_create_time', 5: 'review_context', 6: 'menu',
+                            7: 'total_star', 8: 'taste_star', 9: 'quantity_star',
+                            10: 'delivery_star', 11: 'image_url'}, inplace=True)
+    df_total = pd.merge(df_main, df_category, how='left', on='restuarant_name')
     current_time = datetime.now()
-    df_total = df_main.join(df_category)
     df_main.to_csv(f'pilot_main_{current_time}.csv', encoding='utf-8')
     df_category.to_csv(f'pilot_category_{current_time}.csv', encoding='utf-8')
     df_total.to_csv(f'pilot_total_{current_time}.csv', encoding='utf-8')
 
-else :
+else:
     print(response.status_code)
