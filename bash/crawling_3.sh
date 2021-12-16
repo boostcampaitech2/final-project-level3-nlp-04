a29878f0c85f0e7fda7909e4277f81acc40f789f
raki-1203 <jfhdzzang@gmail.com>
2021-12-17 02:49:35 +0900

    shell 파일 path 설정부분 "" 추가해줘야 설정되는거라서 수정해줬음

--- bash/crawling_3.sh
+++ bash/crawling_3.sh
@@ -2,7 +2,7 @@ echo "crontab start"
 
 pkill chrome
 
-export PYTHONPATH=$PATHONPATH:/opt/ml/final_project/
+export PYTHONPATH="${PATHONPATH}:/opt/ml/final_project/"
 
 cd /opt/ml/final_project
 /opt/conda/envs/lightweight/bin/python3 /opt/ml/final_project/crawling/new_review_crawling.py --num 3
