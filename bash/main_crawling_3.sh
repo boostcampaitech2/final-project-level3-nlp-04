988dc9d08052b63f37faf634f16147e9819fdc76
pseeej <pseeej@gmail.com>
2021-12-17 03:01:55 +0900

    crawling_code modified

--- bash/main_crawling_3.sh
+++ bash/main_crawling_3.sh
@@ -2,7 +2,7 @@ crontab -r
 
 mkdir -p /opt/ml/final-project-level3-nlp-04/bash/log
 
-(crontab -l 2>/dev/null; echo "0 2 * * * bash /opt/ml/final-project-level3-nlp-04/bash/crawling_3.sh  >> /opt/ml/final-project-level3-nlp-04/bash/log/crawling_3.log 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "25 2 * * * bash /opt/ml/final-project-level3-nlp-04/bash/crawling_3.sh  >> /opt/ml/final-project-level3-nlp-04/bash/log/crawling_3.log 2>&1") | crontab -
 
 service cron restart
 crontab -l
