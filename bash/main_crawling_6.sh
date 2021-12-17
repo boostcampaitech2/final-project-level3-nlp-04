59d8d7a1b8b0769044845e6fedd7cecfeb54b5aa
pseeej <pseeej@gmail.com>
2021-12-18 02:09:27 +0900

    changed dir name for me

--- bash/main_crawling_6.sh
+++ bash/main_crawling_6.sh
@@ -1,8 +1,8 @@
 crontab -r
 
-mkdir -p /opt/ml/final_project/bash/log
+mkdir -p /opt/ml/final-project-level3-nlp-04/bash/log
 
-(crontab -l 2>/dev/null; echo "0 2 * * * bash /opt/ml/final_project/bash/crawling_6.sh  >> /opt/ml/final_project/bash/log/crawling_6.log 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "0 2 * * * bash /opt/ml/final-project-level3-nlp-04/bash/crawling_6.sh  >> /opt/ml/final-project-level3-nlp-04/bash/log/crawling_6.log 2>&1") | crontab -
 
 service cron restart
 crontab -l
