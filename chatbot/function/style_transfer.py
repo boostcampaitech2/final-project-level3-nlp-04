11de8dfc4ba22da2a47aa95450f9d23b0b41c65f
sangjun_Leee <inter_10@naver.com>
2021-12-24 05:37:27 +0900

    2

---
+++ chatbot/function/style_transfer.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import torch
+from kobart import get_kobart_tokenizer
+from transformers.models.bart import BartForConditionalGeneration
+
+sys.path.insert(0, "/opt/ml/final-project-level3-nlp-04/style_transfer")
+import style_transfer
+
+def review_transfer(review_text):
+    model = BartForConditionalGeneration.from_pretrained('style_transfer')
+    tokenizer = get_kobart_tokenizer()
+
+
+    text = review_text
+
+    input_ids = tokenizer.encode(text)
+    input_ids = torch.tensor(input_ids)
+    input_ids = input_ids.unsqueeze(0)
+    output = model.generate(input_ids, eos_token_id=1, max_length=512, num_beams=5)
+    output = tokenizer.decode(output[0], skip_special_tokens=True)
+
+    return output
